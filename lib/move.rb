502fe4f2b832481ca13f79a770896b1219bc4768
curious-analyst-4243 <jeremycperry@gmail.com>
2021-02-21 03:08:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(space)
+def input_to_index(user_input)
   puts "What move would you like to make"
   user_input = user_input.to_i -1
 end
