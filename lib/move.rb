e6bfd8b6dde3aed3dfccf8bb3cf50e135de16d0d
curious-analyst-4243 <jeremycperry@gmail.com>
2021-02-21 03:06:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,6 +9,5 @@ end
 # code your input_to_index and move method here!
 def input_to_index(space)
   puts "What move would you like to make"
-  user_input = space.strip
-  converted_input = user_input.to_i -1
+  user_input  user_input.to_i -1
 end
